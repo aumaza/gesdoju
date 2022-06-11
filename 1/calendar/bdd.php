3ed5ca99d52fd78ae47f250a1e28f062f4ee5eb6
Augusto Maza <debianmaza@gmail.com>
2022-06-11 14:39:28 -0300

    update #57 11/06/2022

--- 1/calendar/bdd.php
+++ 1/calendar/bdd.php
@@ -1,7 +1,8 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=gesdoju;charset=utf8', 'root', 'slack142');
+	//$bdd = new PDO('mysql:host=localhost;dbname=gesdoju;charset=utf8', 'root', 'slack142');
+	$bdd = new PDO('mysql:host=localhost;dbname=gesdoju;charset=utf8', 'gesdoju', 'gesodju');
 }
 catch(Exception $e)
 {
