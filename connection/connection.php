951e79673497cc916696c5a117f038f366f2e122
Augusto Maza <debianmaza@gmail.com>
2023-02-02 18:55:01 -0300

    update #76 se cambiaron los datos de conexion a la base de datos

--- connection/connection.php
+++ connection/connection.php
@@ -1,21 +1,21 @@
 <?php
 
 // CONNECTION PRODUCTION
-/*
+
 $dbhost = 'localhost';
 $dbuser = 'gesdoju';
 $dbpass = 'gesdoju';
 $dbase = 'gesdoju';
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbase);
-*/
-// CONNECTION PC OFI
 
+// CONNECTION PC OFI
+/*
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'slack142';
 $dbase = 'gesdoju';
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbase);
-
+*/
 // CONENCTION HOME PC
 /*
 $dbhost = 'slackzone.ddns.net';
