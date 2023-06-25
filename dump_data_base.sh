9c47d8b259f0c37a55c2db4ace819c0c9fc5a155
Augusto Maza <debianmaza@gmail.com>
2023-06-25 20:04:20 -0300

    update #89 25-06-2023 se cambia la linea de conexion en dump_database

--- dump_data_base.sh
+++ dump_data_base.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 fecha=`date +%d-%m-%Y`
 archivo="sqls/gesdoju-$fecha.sql"
-mysqldump --user=root --password=slack142 --host=slackzone.ddns.net gesdoju > $archivo
+#mysqldump --user=root --password=slack142 --host=slackzone.ddns.net gesdoju > $archivo
+mysqldump --user=root --password=slack142 --host=localhost gesdoju > $archivo
 chmod 777 $archivo
 
 
