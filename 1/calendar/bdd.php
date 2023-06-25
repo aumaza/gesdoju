4f4fdc436b6772a55e6dab676e2219d6b08df243
Augusto Maza <debianmaza@gmail.com>
2023-06-25 14:25:09 -0300

    update #87 se cambia url de conexion del calendario

--- 1/calendar/bdd.php
+++ 1/calendar/bdd.php
@@ -1,8 +1,8 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=slackzone.ddns.net;dbname=gesdoju;charset=utf8', 'root', 'slack142');
-	//$bdd = new PDO('mysql:host=localhost;dbname=gesdoju;charset=utf8', 'gesdoju', 'gesdoju');
+	//$bdd = new PDO('mysql:host=slackzone.ddns.net;dbname=gesdoju;charset=utf8', 'root', 'slack142');
+	$bdd = new PDO('mysql:host=localhost;dbname=gesdoju;charset=utf8', 'root', 'slack142');
 }
 catch(Exception $e)
 {
